63851938982e242ecb73cd643ff0ce6477ba8f56
Brenton Horne <brentonhorne77@gmail.com>
2022-01-11 16:02:53 +1000

    Adding comments and keeping to 80 char limit

--- computeTrajectory/back.py
+++ computeTrajectory/back.py
@@ -3,8 +3,10 @@ import rebound, argparse, os
 import numpy as np
 
 # Args
-parser = argparse.ArgumentParser(description='Simulate the trajectory for the specified Svea family asteroid')
-parser.add_argument('integers', metavar='no', type=int, nargs='+', help='an integer specifying the binary file used')
+desc = 'Simulate the trajectory for the specified Svea family asteroid'
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument('integers', metavar='no', type=int, nargs='+', 
+help='an integer specifying the binary file used')
 args = parser.parse_args()
 no = args.integers[0]
 
